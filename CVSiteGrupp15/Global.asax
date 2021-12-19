3d2f549d105e0bae27927e23b55cec6a5c69ba30
alrydb <alrydb123@protonmail.com>
2021-12-19 21:00:08 +0100

    Add project files.

---
+++ CVSiteGrupp15/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CVSiteGrupp15.MvcApplication" Language="C#" %>
---
+++ CVSiteGrupp15/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CVSiteGrupp15.Startup))]
+namespace CVSiteGrupp15
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
